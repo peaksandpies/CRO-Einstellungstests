d7d663b09ec1d9f391c439fdffd767e50d8d7ecd
minu <minu@peaksandpies.com>
2024-06-11 17:42:47 +0200

    init

---
+++ Junior/junior-vorlage.js
@@ -0,0 +1,34 @@
+const newJobsData = [
+  {
+    id: 'pp-senior',
+    title: 'Senior Fullstack Developer (w/m/d)',
+    department: 'Conversion Optimization',
+    link: 'https://peaksandpies.com/'
+  },
+  {
+    id: 'test',
+    title: 'Testdata - do not use',
+    department: null,
+    link: null
+  },
+  {
+    id: 'pp-junior',
+    title: 'Junior Frontend Developer (w/m/d)',
+    department: 'Conversion Optimization',
+    link: '#'
+  }
+]
+
+const generateNewJobsHTML = (data) => {
+  // todo generate and return html using newJobsData
+}
+
+// todo add new generated jobs to the list
+
+// todo implement changes
+
+// css styles bitte hier in den <style> tag schreiben:
+document.body.insertAdjacentHTML('beforeend', /* html */`
+  <style>
+  </style>
+`)
---
+++ Werkstudenten/werkstudent-vorlage.js
@@ -0,0 +1,9 @@
+// todo elemente auswählen
+
+// todo anpassungen machen
+
+// css styles bitte hier in den <style> tag schreiben:
+document.body.insertAdjacentHTML('beforeend', /* html */`
+  <style>
+  </style>
+`)
